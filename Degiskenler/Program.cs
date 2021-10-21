dbbba293b956f9aaf49817c527e053516f288f7a
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-21 14:54:53 +0300

    Add project files.

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TAD - Tip Ad = Değer
+            //Tam sayı değişken tipleri
+            // camelCase PascalCase
+            // degisken ne için kullanılıyorsa onu ifade edecek şekilde isimlenidirin
+            /*
+             * degiskenler sayi ile başlayamaz
+             * _ harici hiçbir özel karakteri kullanamazsınız
+             * boşluk kullanamazsınız
+             * özel anahtar kelimeleri kullanamazsınız
+             * büyükharf-küçük harf duyarlıdır.
+             *
+             */
+
+
+            byte degiskenByte = 0;
+            degiskenByte = Byte.MaxValue;
+            degiskenByte = byte.MinValue;
+
+            short degiskenShort = 153;
+            degiskenShort = Int16.MaxValue;
+            degiskenShort = short.MinValue;
+
+            int degiskenInt = int.MaxValue;
+
+            long degiskenLong = long.MaxValue;
+
+            // ushort uint ulong
+
+            // implicity convert
+
+            short sayi = 32;
+
+            int sayi2 = 32;
+
+            sayi = Convert.ToInt16(sayi2);
+            sayi = (short)sayi2;
+
+            //ondalıklı sayılar
+            double degiskenDouble = Double.MinValue;
+            float degiskenFloat = float.MaxValue;
+            decimal degiskenDecimal = decimal.MaxValue;
+
+            degiskenDouble = 3.14d;
+            degiskenFloat = 3.14F;
+            degiskenDecimal = 3.14m;
+
+            double sonuc = 7 / 2d;
+            Console.WriteLine(sonuc);
+
+            // string kelime
+
+            char karakter = 'a';
+
+            string kelime = "Merhaba Dünya";
+
+            char harf = kelime[5];
+
+            Console.WriteLine();
+
+            //mantıksal değişken tipi
+
+            bool isActive = true;
+
+            isActive = !(degiskenShort == degiskenInt & degiskenDouble != 3.14);
+
+            isActive = !isActive;
+
+            /*
+             * > < >= <= == != ! & |
+             */
+
+            DateTime tarih = DateTime.UtcNow;
+            DayOfWeek haftaninGunu = tarih.DayOfWeek;
+
+            Console.WriteLine(tarih);
+
+            object obj = degiskenShort;
+            obj = degiskenDouble;
+            obj = kelime;
+            obj = karakter;
+            obj = haftaninGunu;
+            obj = tarih;
+
+        }
+    }
+}
---
+++ KararYapilariVeDonguler/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace KararYapilariVeDonguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if 
+            bool isActive = true;
+            if (isActive)
+            {
+                Console.WriteLine("Program aktif");
+            }
+            else
+            {
+                Console.WriteLine("Program Pasif");
+            }
+
+            if (true) Console.WriteLine("1");
+            else Console.WriteLine("2");
+
+            //if if-else else
+            int ortalama = 60;
+
+            if (ortalama >= 50)
+            {
+                Console.WriteLine("Geçtiniz");
+            }
+            else if (ortalama >= 40)
+            {
+                Console.WriteLine("Şartlı geçtiniz");
+            }
+            else
+            {
+                Console.WriteLine("Kaldınız");
+            }
+
+            //ternary if 
+
+            string sonuc = "";
+
+            if (ortalama > 50)
+                sonuc = "Geçtiniz";
+            else
+                sonuc = "Kaldınız";
+
+            // degisken = ifade ? true olduğunda yapılacak : false olduğunda yapılacak
+
+            sonuc = ortalama > 50 ? "Geçtiniz" : "Kaldınız";
+
+            DateTime tarih = DateTime.Now;
+            if (tarih.DayOfWeek == DayOfWeek.Sunday)
+            {
+                sonuc = "Pazar";
+            }
+            else if (tarih.DayOfWeek == DayOfWeek.Monday)
+            {
+                sonuc = "Pazartesi";
+            }
+            else if (tarih.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                sonuc = "Salı";
+            }
+            else if (tarih.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                sonuc = "Çarşamba";
+            }
+
+            switch (sonuc)
+            {
+                case "Pazartesi":
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case "Salı":
+                    Console.WriteLine("Salı");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+
+            DayOfWeek haftaninGunu = tarih.DayOfWeek;
+            switch (haftaninGunu)
+            {
+                case DayOfWeek.Sunday:
+                    sonuc = "Pazar";
+                    break;
+                case DayOfWeek.Monday:
+                    sonuc = "Pazartesi";
+                    break;
+                case DayOfWeek.Tuesday:
+                    sonuc = "Salı";
+                    break;
+                case DayOfWeek.Wednesday:
+                    sonuc = "Çarşamba";
+                    break;
+                case DayOfWeek.Thursday:
+                    sonuc = "Perşembe";
+                    break;
+                case DayOfWeek.Friday:
+                    sonuc = "Cuma";
+                    break;
+                default:
+                    sonuc = "Cumartesi";
+                    break;
+            }
+            //donguler
+            //while
+            int sayac = 0, gunSayisi = 0;
+            while (gunSayisi < 100)
+            {
+                DateTime sonraki = tarih.AddDays(sayac);
+                sayac++;
+                if (sonraki.DayOfWeek == DayOfWeek.Saturday || sonraki.DayOfWeek == DayOfWeek.Sunday)
+                    continue;
+                Console.WriteLine(sonraki);
+                gunSayisi++;
+            }
+
+            sayac = 0; gunSayisi = 0;
+            do
+            {
+
+            } while (gunSayisi < 100);
+        }
+    }
+}
