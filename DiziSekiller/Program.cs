55be13973a52554eba252317aac9f11c2c957876
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-25 11:29:56 +0300

    matris ile içi boş kare çizimi

--- DiziSekiller/Program.cs
+++ DiziSekiller/Program.cs
@@ -6,7 +6,46 @@ namespace DiziSekiller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool[,] matris = null;
+            try
+            {
+                Console.Write("Lütfen Boyutu Girin: ");
+                int girilenBoyut = int.Parse(Console.ReadLine());
+                matris = new bool[girilenBoyut, girilenBoyut];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            for (int satir = 0; satir < matris.GetLength(0); satir++)
+            {
+                for (int sutun = 0; sutun < matris.GetLength(1); sutun++)
+                {
+                    if (satir == 0 || sutun == 0 | satir == matris.GetLength(0) - 1 ||
+                        sutun == matris.GetLength(1) - 1)
+                    {
+                        matris[satir, sutun] = true;
+                    }
+                }
+            }
+
+            //ekrana yazdır
+
+            for (int satir = 0; satir < matris.GetLength(0); satir++)
+            {
+                for (int sutun = 0; sutun < matris.GetLength(1); sutun++)
+                {
+                    if (matris[satir, sutun])
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
