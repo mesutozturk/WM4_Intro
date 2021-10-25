1371efa20380160d943b8b043ab6e9db6742b772
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-25 12:02:15 +0300

    üçgen

--- DiziSekiller/Program.cs
+++ DiziSekiller/Program.cs
@@ -11,20 +11,32 @@ namespace DiziSekiller
             {
                 Console.Write("Lütfen Boyutu Girin: ");
                 int girilenBoyut = int.Parse(Console.ReadLine());
-                matris = new bool[girilenBoyut, girilenBoyut];
+                matris = new bool[girilenBoyut, 2 * girilenBoyut - 1];
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            for (int satir = 0; satir < matris.GetLength(0); satir++)
+
+            int x = matris.GetLength(0);
+            int y = matris.GetLength(1);
+            for (int satir = x - 1; satir >= 0; satir--)
             {
-                for (int sutun = 0; sutun < matris.GetLength(1); sutun++)
+                for (int sutun = 0; sutun < y; sutun++)
                 {
-                    if (satir == 0 || sutun == 0 | satir == matris.GetLength(0) - 1 ||
-                        sutun == matris.GetLength(1) - 1)
+                    matris[satir, sutun] = true;
+                    int boslukSayisi = (x - 1 - satir) * 2;
+
+                    int bosluk2 = boslukSayisi / 2;
+                    if (bosluk2 == 0) continue;
+                    for (int i = 0; i < bosluk2; i++)
                     {
-                        matris[satir, sutun] = true;
+                        matris[satir, i] = false;
+                    }
+
+                    for (int i = y - 1; i > y - 1 - bosluk2; i--)
+                    {
+                        matris[satir, i] = false;
                     }
                 }
             }
@@ -35,14 +47,7 @@ namespace DiziSekiller
             {
                 for (int sutun = 0; sutun < matris.GetLength(1); sutun++)
                 {
-                    if (matris[satir, sutun])
-                    {
-                        Console.Write("X");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
+                    Console.Write(matris[satir, sutun] ? "X" : " ");
                 }
                 Console.WriteLine();
             }
