f7e255b602b4af386fc9b22251cfab1f9a2a74d4
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-26 09:04:58 +0300

    sayı tahmin oyunu

---
+++ SayiTahminOyunu/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do//1 oyunluk döngü
+            {
+                Random rnd = new Random();
+                int tahminSayisi = 0, rastgeleSayi = rnd.Next(1, 101), giris = 0;
+                do
+                {
+                    Console.Write("Rastgele sayımı tahmin et: ");
+                    try
+                    {
+                        giris = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Yanlış bir ifade girdiniz");
+                        continue;
+                    }
+                    finally
+                    {
+                        tahminSayisi++;
+                    }
+                    
+                    if (giris == rastgeleSayi)
+                    {
+                        Console.WriteLine("Tebrikler " + tahminSayisi + ". Denemede Bildiniz");
+                        break;
+                    } else if (giris > rastgeleSayi)
+                    {
+                        Console.WriteLine("Tahminizi Düşürün");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tahminizi Arttırın");
+                    }
+                } while (true);
+
+                Console.WriteLine("Tekrar oynamak için E tuşuna basınız");
+                ConsoleKeyInfo info = Console.ReadKey();
+                if (info.Key != ConsoleKey.E)
+                    break;
+            } while (true);
+        }
+    }
+}
