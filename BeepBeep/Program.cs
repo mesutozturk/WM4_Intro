88d57a2bb9b2ac582e996c9436d1644a96a2fa66
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-26 09:43:14 +0300

    m i

---
+++ BeepBeep/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace BeepBeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(554, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(523, 1200);
+            Thread.Sleep(150);
+            Console.Beep(466, 150);
+            Console.Beep(523, 150);
+        }
+    }
+}
