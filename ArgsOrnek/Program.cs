ab50a1a68454083eac2a84f983978442614035e0
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-10-26 14:21:19 +0300

    args.

---
+++ ArgsOrnek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArgsOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
