fb304fb373b1bd949df46954eac0c14c38b99511
guney.yunus.dev@outlook.com <guney.yunus.dev@outlook.com>
2021-11-04 14:11:14 +0300

    arayüz günellemeleri

--- PaymentMethod/Form1.Designer.cs
+++ PaymentMethod/Form1.Designer.cs
@@ -32,15 +32,6 @@ namespace PaymentMethod
             this.components = new System.ComponentModel.Container();
             this.cmbPaymentMethod = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtCvv = new System.Windows.Forms.TextBox();
-            this.cmbYil = new System.Windows.Forms.ComboBox();
-            this.cmbAy = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtAdSoyad = new System.Windows.Forms.TextBox();
-            this.txtCardNumber = new System.Windows.Forms.TextBox();
             this.lstTaksitler = new System.Windows.Forms.ListBox();
             this.btnOdemeYap = new System.Windows.Forms.Button();
             this.creditCardBox1 = new PaymentMethod.UserControls.CreditCardBox();
@@ -62,15 +53,7 @@ namespace PaymentMethod
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.txtCvv);
-            this.groupBox1.Controls.Add(this.cmbYil);
-            this.groupBox1.Controls.Add(this.cmbAy);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.txtAdSoyad);
-            this.groupBox1.Controls.Add(this.txtCardNumber);
+            this.groupBox1.Controls.Add(this.creditCardBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 76);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(344, 134);
@@ -78,97 +61,20 @@ namespace PaymentMethod
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Kart Bilgileri";
             // 
-            // txtCvv
-            // 
-            this.txtCvv.Location = new System.Drawing.Point(273, 89);
-            this.txtCvv.Name = "txtCvv";
-            this.txtCvv.PlaceholderText = "000";
-            this.txtCvv.Size = new System.Drawing.Size(55, 27);
-            this.txtCvv.TabIndex = 5;
-            // 
-            // cmbYil
-            // 
-            this.cmbYil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbYil.FormattingEnabled = true;
-            this.cmbYil.Location = new System.Drawing.Point(162, 88);
-            this.cmbYil.Name = "cmbYil";
-            this.cmbYil.Size = new System.Drawing.Size(52, 28);
-            this.cmbYil.TabIndex = 4;
-            // 
-            // cmbAy
-            // 
-            this.cmbAy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbAy.FormattingEnabled = true;
-            this.cmbAy.Location = new System.Drawing.Point(104, 88);
-            this.cmbAy.Name = "cmbAy";
-            this.cmbAy.Size = new System.Drawing.Size(52, 28);
-            this.cmbAy.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 91);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Son Kull. Tar.";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(231, 91);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(36, 20);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "CVV";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Ad Soyad";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Kart No";
-            // 
-            // txtAdSoyad
-            // 
-            this.txtAdSoyad.Location = new System.Drawing.Point(104, 55);
-            this.txtAdSoyad.Name = "txtAdSoyad";
-            this.txtAdSoyad.Size = new System.Drawing.Size(224, 27);
-            this.txtAdSoyad.TabIndex = 1;
-            // 
-            // txtCardNumber
-            // 
-            this.txtCardNumber.Location = new System.Drawing.Point(104, 20);
-            this.txtCardNumber.Name = "txtCardNumber";
-            this.txtCardNumber.PlaceholderText = "0000 0000 0000 0000";
-            this.txtCardNumber.Size = new System.Drawing.Size(224, 27);
-            this.txtCardNumber.TabIndex = 0;
-            // 
             // lstTaksitler
             // 
             this.lstTaksitler.FormattingEnabled = true;
             this.lstTaksitler.ItemHeight = 20;
-            this.lstTaksitler.Location = new System.Drawing.Point(362, 24);
+            this.lstTaksitler.Location = new System.Drawing.Point(364, 26);
             this.lstTaksitler.Name = "lstTaksitler";
             this.lstTaksitler.Size = new System.Drawing.Size(233, 264);
             this.lstTaksitler.TabIndex = 2;
             // 
             // btnOdemeYap
             // 
-            this.btnOdemeYap.Location = new System.Drawing.Point(362, 294);
+            this.btnOdemeYap.Location = new System.Drawing.Point(12, 210);
             this.btnOdemeYap.Name = "btnOdemeYap";
-            this.btnOdemeYap.Size = new System.Drawing.Size(233, 80);
+            this.btnOdemeYap.Size = new System.Drawing.Size(337, 80);
             this.btnOdemeYap.TabIndex = 3;
             this.btnOdemeYap.Text = "Ödeme Yap";
             this.btnOdemeYap.UseVisualStyleBackColor = true;
@@ -178,12 +84,12 @@ namespace PaymentMethod
             // 
             this.creditCardBox1.AdSoyad = "";
             this.creditCardBox1.Cvv = "";
-            this.creditCardBox1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.creditCardBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.creditCardBox1.KartNo = "               ";
-            this.creditCardBox1.Location = new System.Drawing.Point(12, 225);
+            this.creditCardBox1.Location = new System.Drawing.Point(7, 26);
             this.creditCardBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.creditCardBox1.Name = "creditCardBox1";
-            this.creditCardBox1.Size = new System.Drawing.Size(343, 93);
+            this.creditCardBox1.Size = new System.Drawing.Size(330, 102);
             this.creditCardBox1.TabIndex = 4;
             this.creditCardBox1.AdSoyadHata += new PaymentMethod.UserControls.AdSoyadHataEvent(this.creditCardBox1_AdSoyadHata);
             // 
@@ -201,7 +107,6 @@ namespace PaymentMethod
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(609, 389);
-            this.Controls.Add(this.creditCardBox1);
             this.Controls.Add(this.btnOdemeYap);
             this.Controls.Add(this.lstTaksitler);
             this.Controls.Add(this.groupBox1);
@@ -210,7 +115,6 @@ namespace PaymentMethod
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
 
@@ -220,15 +124,6 @@ namespace PaymentMethod
 
         private System.Windows.Forms.ComboBox cmbPaymentMethod;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox txtCvv;
-        private System.Windows.Forms.ComboBox cmbYil;
-        private System.Windows.Forms.ComboBox cmbAy;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtAdSoyad;
-        private System.Windows.Forms.TextBox txtCardNumber;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ListBox lstTaksitler;
         private System.Windows.Forms.Button btnOdemeYap;
         private UserControls.CreditCardBox creditCardBox1;
