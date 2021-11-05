5b1939cd2c3ba0a024e3d4546ddd93387467c131
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-05 12:07:35 +0300

    xml json içeri-dışarı aktar işlemleri ok

--- KisiEnvanter/Form1.Designer.cs
+++ KisiEnvanter/Form1.Designer.cs
@@ -41,30 +41,40 @@ namespace KisiEnvanter
             this.txtAd = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pbResim = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaİşlemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dışarıAktarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.içeriAktarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.jSONDışarıAktarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jSONİçeriAktarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGuncelle
             // 
-            this.btnGuncelle.Location = new System.Drawing.Point(258, 231);
+            this.btnGuncelle.Location = new System.Drawing.Point(258, 261);
             this.btnGuncelle.Name = "btnGuncelle";
             this.btnGuncelle.Size = new System.Drawing.Size(118, 74);
             this.btnGuncelle.TabIndex = 16;
             this.btnGuncelle.Text = "Güncelle";
             this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
             // lstKisiler
             // 
             this.lstKisiler.FormattingEnabled = true;
             this.lstKisiler.ItemHeight = 20;
-            this.lstKisiler.Location = new System.Drawing.Point(382, 9);
+            this.lstKisiler.Location = new System.Drawing.Point(382, 39);
             this.lstKisiler.Name = "lstKisiler";
             this.lstKisiler.Size = new System.Drawing.Size(311, 444);
             this.lstKisiler.TabIndex = 15;
+            this.lstKisiler.SelectedIndexChanged += new System.EventHandler(this.lstKisiler_SelectedIndexChanged);
             // 
             // btnKaydet
             // 
-            this.btnKaydet.Location = new System.Drawing.Point(258, 142);
+            this.btnKaydet.Location = new System.Drawing.Point(258, 172);
             this.btnKaydet.Name = "btnKaydet";
             this.btnKaydet.Size = new System.Drawing.Size(118, 74);
             this.btnKaydet.TabIndex = 14;
@@ -74,14 +84,14 @@ namespace KisiEnvanter
             // 
             // dtpDogumTarihi
             // 
-            this.dtpDogumTarihi.Location = new System.Drawing.Point(126, 109);
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(126, 139);
             this.dtpDogumTarihi.Name = "dtpDogumTarihi";
             this.dtpDogumTarihi.Size = new System.Drawing.Size(250, 27);
             this.dtpDogumTarihi.TabIndex = 13;
             // 
             // txtTelefon
             // 
-            this.txtTelefon.Location = new System.Drawing.Point(126, 75);
+            this.txtTelefon.Location = new System.Drawing.Point(126, 105);
             this.txtTelefon.Name = "txtTelefon";
             this.txtTelefon.Size = new System.Drawing.Size(250, 27);
             this.txtTelefon.TabIndex = 10;
@@ -89,7 +99,7 @@ namespace KisiEnvanter
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.Location = new System.Drawing.Point(12, 144);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 20);
             this.label4.TabIndex = 6;
@@ -98,7 +108,7 @@ namespace KisiEnvanter
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Location = new System.Drawing.Point(12, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 20);
             this.label3.TabIndex = 7;
@@ -106,7 +116,7 @@ namespace KisiEnvanter
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(126, 42);
+            this.txtSoyad.Location = new System.Drawing.Point(126, 72);
             this.txtSoyad.Name = "txtSoyad";
             this.txtSoyad.Size = new System.Drawing.Size(250, 27);
             this.txtSoyad.TabIndex = 11;
@@ -114,7 +124,7 @@ namespace KisiEnvanter
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Location = new System.Drawing.Point(12, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 20);
             this.label2.TabIndex = 8;
@@ -122,7 +132,7 @@ namespace KisiEnvanter
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(126, 9);
+            this.txtAd.Location = new System.Drawing.Point(126, 39);
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(250, 27);
             this.txtAd.TabIndex = 12;
@@ -130,7 +140,7 @@ namespace KisiEnvanter
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(12, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 20);
             this.label1.TabIndex = 9;
@@ -138,7 +148,7 @@ namespace KisiEnvanter
             // 
             // pbResim
             // 
-            this.pbResim.Location = new System.Drawing.Point(12, 142);
+            this.pbResim.Location = new System.Drawing.Point(12, 172);
             this.pbResim.Name = "pbResim";
             this.pbResim.Size = new System.Drawing.Size(240, 312);
             this.pbResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -146,11 +156,67 @@ namespace KisiEnvanter
             this.pbResim.TabStop = false;
             this.pbResim.Click += new System.EventHandler(this.pbResim_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaİşlemleriToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(708, 28);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaİşlemleriToolStripMenuItem
+            // 
+            this.dosyaİşlemleriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dışarıAktarToolStripMenuItem,
+            this.içeriAktarToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.jSONDışarıAktarToolStripMenuItem,
+            this.jSONİçeriAktarToolStripMenuItem});
+            this.dosyaİşlemleriToolStripMenuItem.Name = "dosyaİşlemleriToolStripMenuItem";
+            this.dosyaİşlemleriToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
+            this.dosyaİşlemleriToolStripMenuItem.Text = "Dosya İşlemleri";
+            // 
+            // dışarıAktarToolStripMenuItem
+            // 
+            this.dışarıAktarToolStripMenuItem.Name = "dışarıAktarToolStripMenuItem";
+            this.dışarıAktarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.dışarıAktarToolStripMenuItem.Text = "XML Dışarı Aktar";
+            this.dışarıAktarToolStripMenuItem.Click += new System.EventHandler(this.dışarıAktarToolStripMenuItem_Click);
+            // 
+            // içeriAktarToolStripMenuItem
+            // 
+            this.içeriAktarToolStripMenuItem.Name = "içeriAktarToolStripMenuItem";
+            this.içeriAktarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.içeriAktarToolStripMenuItem.Text = "XML İçeri Aktar";
+            this.içeriAktarToolStripMenuItem.Click += new System.EventHandler(this.içeriAktarToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // jSONDışarıAktarToolStripMenuItem
+            // 
+            this.jSONDışarıAktarToolStripMenuItem.Name = "jSONDışarıAktarToolStripMenuItem";
+            this.jSONDışarıAktarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.jSONDışarıAktarToolStripMenuItem.Text = "JSON Dışarı Aktar";
+            this.jSONDışarıAktarToolStripMenuItem.Click += new System.EventHandler(this.jSONDışarıAktarToolStripMenuItem_Click);
+            // 
+            // jSONİçeriAktarToolStripMenuItem
+            // 
+            this.jSONİçeriAktarToolStripMenuItem.Name = "jSONİçeriAktarToolStripMenuItem";
+            this.jSONİçeriAktarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.jSONİçeriAktarToolStripMenuItem.Text = "JSON İçeri Aktar";
+            this.jSONİçeriAktarToolStripMenuItem.Click += new System.EventHandler(this.jSONİçeriAktarToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 466);
+            this.ClientSize = new System.Drawing.Size(708, 501);
             this.Controls.Add(this.pbResim);
             this.Controls.Add(this.btnGuncelle);
             this.Controls.Add(this.lstKisiler);
@@ -163,9 +229,13 @@ namespace KisiEnvanter
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtAd);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,6 +255,13 @@ namespace KisiEnvanter
         private System.Windows.Forms.TextBox txtAd;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbResim;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaİşlemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dışarıAktarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem içeriAktarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem jSONDışarıAktarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jSONİçeriAktarToolStripMenuItem;
     }
 }
 
--- KisiEnvanter/Form1.cs
+++ KisiEnvanter/Form1.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace KisiEnvanter
 {
@@ -61,6 +60,116 @@ namespace KisiEnvanter
             }
             kisiler.Add(yeniKisi);
             ListeyiDoldur();
+        }
+
+        private Kisi seciliKisi;
+        private void lstKisiler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstKisiler.SelectedItem == null) return;
+
+            seciliKisi = lstKisiler.SelectedItem as Kisi;
+
+            txtAd.Text = seciliKisi.Ad;
+            txtSoyad.Text = seciliKisi.Soyad;
+            txtTelefon.Text = seciliKisi.Telefon;
+            dtpDogumTarihi.Value = seciliKisi.DogumTarihi;
+
+            if (seciliKisi.Fotograf != null)
+            {
+                MemoryStream stream = new MemoryStream(seciliKisi.Fotograf);
+                pbResim.Image = Image.FromStream(stream);
+            }
+        }
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            seciliKisi.Ad = txtAd.Text;
+            seciliKisi.Soyad = txtSoyad.Text;
+            seciliKisi.Telefon = txtTelefon.Text;
+
+            if (pbResim.Image != null)
+            {
+                MemoryStream resimStream = new MemoryStream();
+                pbResim.Image.Save(resimStream, ImageFormat.Jpeg);
+
+                seciliKisi.Fotograf = resimStream.ToArray();
+            }
+            ListeyiDoldur();
+        }
+
+        private void dışarıAktarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Dışarı aktar";
+            dialog.Filter = "XML Format | *.xml";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Kisi>));
+                TextWriter writer = new StreamWriter(dialog.FileName);
+                serializer.Serialize(writer, kisiler);
+                writer.Close();
+                MessageBox.Show($"{kisiler.Count} adet kişi dışarı aktarıldı.");
+            }
+        }
+
+        private void içeriAktarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "Bir XML dosyası seçiniz";
+            dialog.Filter = "XML | *.xml";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Kisi>));
+                XmlTextReader reader = new XmlTextReader(dialog.FileName);
+                kisiler = (List<Kisi>)serializer.Deserialize(reader);
+                MessageBox.Show($"{kisiler.Count} adet kişi içeri aktarıldı");
+                ListeyiDoldur();
+            }
+        }
+
+        private void jSONDışarıAktarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Title = "Dışarı aktar";
+            dialog.Filter = "JSON Format | *.json";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileStream fileStream = new FileStream(dialog.FileName, FileMode.OpenOrCreate);
+                StreamWriter writer = new StreamWriter(fileStream);
+                writer.Write(JsonConvert.SerializeObject(kisiler, Formatting.Indented));
+                writer.Close();
+                writer.Dispose();
+                MessageBox.Show($"{kisiler.Count} adet kişi dışarı aktarıldı.");
+            }
+        }
+
+        private void jSONİçeriAktarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "Bir JSON dosyası seçiniz";
+            dialog.Filter = "JSON | *.json";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileStream fileStream = new FileStream(dialog.FileName, FileMode.Open);
+                StreamReader reader = new StreamReader(fileStream);
+                string dosyaIcerigi = reader.ReadToEnd();
+                kisiler = JsonConvert.DeserializeObject<List<Kisi>>(dosyaIcerigi);
+                MessageBox.Show($"{kisiler.Count} adet kişi içeri aktarıldı");
+                ListeyiDoldur();
+            }
         }
     }
 }
