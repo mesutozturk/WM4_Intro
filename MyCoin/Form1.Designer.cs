c38e2019d0608ea6f7e73fdbe5aaa57632b538f1
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-08 11:53:42 +0300

    coin pt5

--- MyCoin/Form1.Designer.cs
+++ MyCoin/Form1.Designer.cs
@@ -29,21 +29,49 @@ namespace MyCoin
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.lstExchangeInfo = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(12, 12);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(267, 27);
+            this.txtAra.TabIndex = 0;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // lstExchangeInfo
+            // 
+            this.lstExchangeInfo.FormattingEnabled = true;
+            this.lstExchangeInfo.ItemHeight = 20;
+            this.lstExchangeInfo.Location = new System.Drawing.Point(12, 45);
+            this.lstExchangeInfo.Name = "lstExchangeInfo";
+            this.lstExchangeInfo.Size = new System.Drawing.Size(267, 584);
+            this.lstExchangeInfo.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 640);
+            this.Controls.Add(this.lstExchangeInfo);
+            this.Controls.Add(this.txtAra);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.ListBox lstExchangeInfo;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- MyCoin/Form1.cs
+++ MyCoin/Form1.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MyCoin.Models;
 using MyCoin.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace MyCoin
 {
@@ -18,13 +15,36 @@ namespace MyCoin
             InitializeComponent();
         }
 
+        private List<Symbol> _symbols = new List<Symbol>();
         private void Form1_Load(object sender, EventArgs e)
         {
             var result = new ExchangeInfoService().Result();
+            _symbols = result.Symbols;
 
-            var result2 = new SymbolTickerService().Result("DOGEUSDT");
+            _symbols = _symbols
+                .Where(x => x.status == "TRADING")
+                .OrderBy(x => x.symbol)
+                .ToList();
 
-            Console.WriteLine();
+            lstExchangeInfo.DataSource = _symbols;
+            lstExchangeInfo.DisplayMember = nameof(Symbol.symbol);
+
+            this.Text = $"{_symbols.Count} Adet Coin Listelenmektedir.";
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAra.Text))
+            {
+                lstExchangeInfo.DataSource = _symbols;
+                this.Text = $"{_symbols.Count} Adet Coin Listelenmektedir.";
+            }
+            else
+            {
+                var result = _symbols.Where(x => x.symbol.Contains(txtAra.Text.ToUpper())).ToList();
+                lstExchangeInfo.DataSource = result;
+                this.Text = $"{result.Count} Adet Coin Listelenmektedir.";
+            }
         }
     }
 }
