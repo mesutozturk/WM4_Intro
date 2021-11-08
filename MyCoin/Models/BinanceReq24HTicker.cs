dc1618e3324b50a7e4deff986eea111277e0d3df
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-08 09:40:37 +0300

    coin pt2

---
+++ MyCoin/Models/BinanceReq24HTicker.cs
@@ -0,0 +1,27 @@
+﻿namespace MyCoin.Models
+{
+    public class BinanceReq24HTicker
+    {
+        public string Symbol { get; set; }
+        public string PriceChange { get; set; }
+        public string PriceChangePercent { get; set; }
+        public string WeightedAvgPrice { get; set; }
+        public string PrevClosePrice { get; set; }
+        public string LastPrice { get; set; }
+        public string LastQty { get; set; }
+        public string BidPrice { get; set; }
+        public string BidQty { get; set; }
+        public string AskPrice { get; set; }
+        public string AskQty { get; set; }
+        public string OpenPrice { get; set; }
+        public string HighPrice { get; set; }
+        public string LowPrice { get; set; }
+        public string Volume { get; set; }
+        public string QuoteVolume { get; set; }
+        public long OpenTime { get; set; }
+        public long CloseTime { get; set; }
+        public int FirstId { get; set; }
+        public int LastId { get; set; }
+        public int Count { get; set; }
+    }
+}
